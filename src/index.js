697dc60bd231256ec4003baee92ab4dc0ac8562e
Arryel-Kauhu <arryel.klug@gmail.com>
2023-02-19 20:54:14 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// const user = {};
+
+// // optional chaining "console.log(user?.address?.street)";
+
+// console.log(user?.address?.street);
+
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1 id='title'>
+    <span>Olá World!!!!</span>
+  </h1>,
+  document.getElementById('root'),
+);
