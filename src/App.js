5d5707afdb27afb0d52b0ce0a85f6d7073b226a0
Arryel-Kauhu <arryel.klug@gmail.com>
2023-02-20 12:30:07 -0300

    O que são Props?

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 //Sempre importar o React nos arquivos que são de componentes
 
 import React from "react";
-
-const category = 'Posts da Semana';
-
-//Render
+import Post from './Post'
 
 function App() {
   return (
   // <React.Fragment> ou <> 
   // Entre chaves o JSX identifica como JS
+
+  //Props (properties)
   <>
     <h1>JStacks</h1>    
-    <h2>{`${category} de 2023`}</h2>    
+    <h2>Posts da semana</h2>       
+    <hr />
+
+    <Post
+      post={{
+        title:'Título da noticia 01',
+        subtitle:'Subtitulo da Noticia 01'
+     }}     
+    />
+    <Post
+      post={{
+        title:'Título da noticia 02',
+        subtitle:'Subtitulo da Noticia 02'
+     }}     
+    /> 
+    <Post 
+      post={{
+        title:'Título da noticia 03',
+        subtitle:'Subtitulo da Noticia 03'
+    }}
+    />
   </>
   );
 };
