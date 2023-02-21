bbf3d60bc0a767aadecfe2f63eca15ae37c39635
Arryel-Kauhu <arryel.klug@gmail.com>
2023-02-21 08:52:52 -0300

    Trabalhando com States

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 //Sempre importar o React nos arquivos que são de componentes
 
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import Header from './Header';
 
-const posts = [
-  { title: 'Title#01', subtitle: 'Sub01', likes:20 },
-  { title: 'Title#02', subtitle: 'Sub02', likes:30 },
-  { title: 'Title#03', subtitle: 'Sub03', likes:40 },
-  { title: 'Title#04', subtitle: 'Sub04', likes:50 },
-  
-];
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub01', likes:20 },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub02', likes:30 },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub03', likes:40 },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub04', likes:50 },
+    
+  ]);
+  function handleRefresh(){  
+    setPosts((prevState) => [
+      ...prevState,
+      {
+        id: Math.random(), 
+        title: `Title#0${prevState.length+1}`, 
+        subtitle: `Sub#0${prevState.length+1}`, 
+        likes:50,
+      } 
+    ]);
+    
+  }
   return (
   // <React.Fragment> ou <> 
   // Entre chaves o JSX identifica como JS
@@ -20,13 +32,16 @@ function App() {
   //Props (properties)
   <>
     <Header> 
-      <h2>Posts da semana</h2>  
+      <h2>
+        Posts da semana
+        <button onClick={handleRefresh}>Atualizar</button>
+      </h2>  
     </Header>
     <hr />   
 
     {posts.map(post => (
       <Post 
-        key={post.title}
+        key={post.id}
         likes={post.likes}
         post ={{
           title: post.title,
